2e411f4a4c5d3c3bea04331c47a325125a2a6922
fernandogatto <fernandogatto17@gmail.com>
2020-05-03 00:06:16 -0300

    Pushing project

---
+++ src/pages/SignUp/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  place-content: center;
+  width: 100%;
+  max-width: 768px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      transition: color .2s;
+
+      &:hover {
+        color: ${shade(.2, '#f4ede8')};
+      }
+
+      svg {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  > a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color .2s;
+
+      &:hover {
+        color: ${shade(.2, '#f4ede8')};
+      }
+
+      svg {
+        margin-right: 16px;
+      }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signUpBackgroundImg}) no-repeat center center;
+  background-size: cover;
+`;
